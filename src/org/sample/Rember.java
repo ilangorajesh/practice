63a0c8b617ef8b7cfe9002633f35e5e246bf620c
ilango451646 <ilango451646@gmail.com>
2022-04-30 20:43:56 +0530

    master

---
+++ src/org/sample/Rember.java
@@ -0,0 +1,20 @@
+package org.sample;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Rember {
+public static void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver","C:\\Users\\HP PC\\eclipse-workspace\\Rember git\\driver\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://www.facebook.com/");
+	driver.manage().window().maximize();
+	WebElement username = driver.findElement(By.id("email"));
+	username.sendKeys("ilango");
+	
+	
+	
+}
+}
